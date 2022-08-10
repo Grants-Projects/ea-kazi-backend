321b76f782815a969ba8edda441ec31c5f7401a5
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-08-10 15:24:32 +0100

    Certificate Route, Service, Repository, Models and Migrations

--- src/database/migrations/1659482205775-Courses.ts
+++ src/database/migrations/1659482205775-Courses.ts
@@ -8,77 +8,77 @@ import {
 export class Courses1659482205775 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         const table = new Table({
-            name: 'course',
-            columns: [
-                {
-                    name: 'id',
-                    type: 'uuid',
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: 'uuid',
-                },
-                {
-                    name: 'author_id',
-                    type: 'uuid',
-                    default: null,
-                },
-                {
-                    name: 'state',
-                    type: 'varchar',
-                    default: null,
-                },
-                {
-                    name: 'status',
-                    type: 'varchar',
-                    default: null,
-                },
-                {
-                    name: 'title',
-                    type: 'varchar',
-                    default: null,
-                },
-                {
-                    name: 'description',
-                    type: 'longtext',
-                    default: null,
-                },
-                {
-                    name: 'metadata',
-                    type: 'varchar',
-                    default: null,
-                },
-                {
-                    name: 'published_by',
-                    type: 'uuid',
-                    default: null,
-                },
-                {
-                    name: 'approved_by',
-                    type: 'uuid',
-                    default: null,
-                },
-                {
-                    name: 'published_at',
-                    type: 'timestamp',
-                    default: null,
-                },
-                {
-                    name: 'approved_at',
-                    type: 'timestamp',
-                    default: null,
-                },
-                {
-                    name: 'created_at',
-                    type: 'timestamp',
-                    default: 'now()',
-                },
-                {
-                    name: 'updated_at',
-                    type: 'timestamp',
-                    default: 'now()',
-                },
-            ],
-        });
+					name: 'course',
+					columns: [
+						{
+							name: 'id',
+							type: 'uuid',
+							isPrimary: true,
+							isGenerated: true,
+							generationStrategy: 'uuid',
+						},
+						{
+							name: 'author_id',
+							type: 'uuid',
+							default: null,
+						},
+						{
+							name: 'state',
+							type: 'varchar',
+							default: null,
+						},
+						{
+							name: 'status',
+							type: 'varchar',
+							default: null,
+						},
+						{
+							name: 'title',
+							type: 'varchar',
+							default: null,
+						},
+						{
+							name: 'description',
+							type: 'longtext',
+							default: null,
+						},
+						{
+							name: 'metadata',
+							type: 'varchar',
+							default: null,
+						},
+						{
+							name: 'published_by',
+							type: 'uuid',
+							default: null,
+						},
+						{
+							name: 'approved_by',
+							type: 'uuid',
+							default: null,
+						},
+						{
+							name: 'published_at',
+							type: 'timestamp',
+							default: null,
+						},
+						{
+							name: 'approved_at',
+							type: 'timestamp',
+							default: null,
+						},
+						{
+							name: 'created_at',
+							type: 'timestamp',
+							default: 'now()',
+						},
+						{
+							name: 'updated_at',
+							type: 'timestamp',
+							default: 'now()',
+						},
+					],
+				});
 
         await queryRunner.createTable(table, true);
 
