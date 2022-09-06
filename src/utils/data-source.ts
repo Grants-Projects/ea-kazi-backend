d65417ba8fee39b55a91f1593cd0927ff5a8146b
marvikomo <marvikomo@gmail.com>
2022-09-06 09:04:04 +0100

    update

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -14,9 +14,9 @@ import { User } from '../models';
 export const AppDataSource = new DataSource({
     host: process.env.MYSQL_HOST || 'localhost',
     port: Number(process.env.MYSQL_PORT) || 3306,
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || '12345678',
-    database: process.env.MYSQL_DATABASE || 'ea_kazi',
+    username: process.env.MYSQL_USER || 'marvelous',
+    password: process.env.MYSQL_PASSWORD || 'marvelous',
+    database: process.env.MYSQL_DATABASE || 'ea-kazi',
     synchronize: false,
     migrationsRun: true,
     type: 'mysql',
